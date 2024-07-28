f0b364e769ecf70a71da06807c2633a6bcf63991
Vladislav Pavlyuk <vladpavliuk@gmail.com>
2024-07-28 10:03:34 +0200

    Watching video solved!

--- MusicLib/Controllers/VideoController.cs
+++ MusicLib/Controllers/VideoController.cs
@@ -26,7 +26,7 @@ namespace MusicLib.Controllers
         }
 
         // GET: Videos/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Watch(int? id)
         {
             try
             {
